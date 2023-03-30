5070bc1953e2815058a6198b91ca6672576c8a1f
Dirghayu Joshi <dirghayujoshi47@gmail.com>
2023-03-30 20:05:18 +0530

    Frontend remove try.

--- backend/middleware/payment.js
+++
@@ -1,8 +0,0 @@
-const catchAsyncError = require("./catchAsyncError")
-const { v4: uuidv4 } = require('uuid');
-
-module.exports.handlePayment = catchAsyncError(async (req, res, next) => {
-    //The code for handling payment will come here.
-    req.body.transID = uuidv4()
-    return next();
-})
