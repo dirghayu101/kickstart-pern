81996e53aba383c8060c51c666798e91fcc7fba9
Dirghayu Joshi <dirghayujoshi47@gmail.com>
2023-03-30 20:00:28 +0530

    Git commit 5. Vercel deploy.

--- backend/app.js
+++ backend/app.js
@@ -15,16 +15,18 @@ const userReservation = require('./routes/userReservation')
 app.use("/api/v1/user", user, userReservation)
 app.use("/api/v1/admin", admin)
 
-/* Error Middleware. 
-This location is crucial because it will come when the next will be invoked from the routes.
-*/
-app.use(errorMiddleware)
-
 // Dummy Route
 app.get('/', (req, res) => {
     console.log("Request received at get '/' route.")
     res.status(200).send('<h1>The website is under construction!</h1>')
 })
+
+/* Error Middleware. 
+This location is crucial because it will come when the next will be invoked from the routes.
+*/
+app.use(errorMiddleware)
+
+
 
 
 module.exports = app
